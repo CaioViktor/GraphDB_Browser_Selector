8bbc9bc961d2843e44be811d1eaea8ca8bfd1322
CaioViktor <caioviktor@hotmail.com>
2022-04-25 11:47:02 -0300

    generalizando consultas salvas

--- static/js/list_itens_saved.js
+++ static/js/list_itens_saved.js
@@ -1,9 +1,19 @@
 const data = d3.json("/query_saved/"+id+"/"+page).then(function(dataR){
 	let data = [];
-	dataR.forEach(function(d){
-		
+	dataR['vars'].forEach(function(d){
+		let row = '<th>';
+		row += d;
+		row += "</th>";
+		$('#elements > thead').append(row);
+	});
+	$('#elements > thead').append('<th>Grafo</th>');
+	dataR['resources'].forEach(function(d){
 		let row = '<tr>';
-		row += '<td title="'+d.graphdb_url+'"><a href="'+d.graphdb_url+'">'+d.label+"</a></td>";
+		for(field in d){
+			if(field != "graphdb_url")
+				row += '<td>'+d[field]+'</td>';	
+		}
+		row += '<td><a target="_blank" href="'+d.graphdb_url+'">Ver</a></td>';
 		row += "</tr>";
 		$('#elements > tbody:last-child').append(row);
 		data.push(d);
