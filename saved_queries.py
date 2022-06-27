03aafaedf839bb847907acb1b6142e6f18ce6de1
Caio Viktor Avila <caioviktor@hotmail.com>
2022-06-27 17:30:57 -0300

    Update saved_queries.py

--- saved_queries.py
+++ saved_queries.py
@@ -220,7 +220,7 @@ UNION
 
 }
 LIMIT 100""",
-    'uri_var':"?empresa",
+    'uri_var':"empresa",
     'construct_query': """
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
