7aae55cdf80134c8302ce05f692de6d2879dae9f
Caio Viktor Avila <caioviktor@hotmail.com>
2022-06-27 15:59:24 -0300

    Update saved_queries.py

--- saved_queries.py
+++ saved_queries.py
@@ -69,71 +69,71 @@
 # }
 
 
-q1 = {
-    'description': """Quais Empresas Não Ativas na RFB mas Ativas na SEFAZ?""",
-    'query': """
-PREFIX sefazma: <http://www.sefaz.ma.gov.br/ontology/>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-SELECT ?estabelecimento_rfb ?nome_fantasia_rfb ?tipo_situacao_rfb ?tipo_situacao_cadastro WHERE {
-    
-  ?estabelecimento_rfb a sefazma:Estabelecimento_QC1_RFB;
-              sefazma:nome_fantasia_rfb ?nome_fantasia_rfb;
-     sefazma:tem_situacao_cadastral ?situacao_cadastral_rfb.
-
-  ?situacao_cadastral_rfb 
-        sefazma:tipo_situacao ?tipo_situacao_rfb.
-    
-  ?estabelecimento_cadastro a sefazma:Estabelecimento_QC1_Cadastro;
-      sefazma:nome_fantasia_cadastro ?nome_fantasia_cadastro;
-      sefazma:tem_situacao_cadastral_cadastro ?situacao_cadastral_cadastro.
-    
-  ?estabelecimento_rfb owl:sameAs ?estabelecimento_cadastro.
-    
-  ?situacao_cadastral_cadastro 
-      sefazma:tipo_situacao ?tipo_situacao_cadastro.
- 
-}
-LIMIT 100""",
-    'uri_var':"estabelecimento_rfb",
-    'construct_query': """
-    PREFIX sefazma: <http://www.sefaz.ma.gov.br/ontology/>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-CONSTRUCT{
-    ?estabelecimento_rfb sefazma:tem_situacao_cadastral ?situacao_cadastral_rfb.
-    ?situacao_cadastral_rfb sefazma:tipo_situacao ?tipo_situacao_rfb.
-    ?estabelecimento_cadastro sefazma:tem_situacao_cadastral ?situacao_cadastral_cadastro.
-    ?situacao_cadastral_cadastro sefazma:tipo_situacao ?tipo_situacao_cadastro.
-    ?estabelecimento_rfb owl:sameAs  ?estabelecimento_cadastro.
-    ?estabelecimento_cadastro owl:sameAs ?estabelecimento_rfb.
-    
-}WHERE {
-  
-  ?estabelecimento_rfb a sefazma:Estabelecimento_RFB;
-     sefazma:tem_situacao_cadastral ?situacao_cadastral_rfb.
-  
-  ?situacao_cadastral_rfb sefazma:tipo_situacao ?tipo_situacao_rfb.
-  
-  ?estabelecimento_cadastro a sefazma:Estabelecimento_Cadastro;
-        sefazma:tem_situacao_cadastral ?situacao_cadastral_cadastro.
-    
-  ?situacao_cadastral_cadastro sefazma:tipo_situacao ?tipo_situacao_cadastro.
-    
-  ?estabelecimento_rfb owl:sameAs  ?estabelecimento_cadastro.
- 
-    FILTER(?estabelecimento_rfb = <$URI>)
-}
-    """
-}
+#q1 = {
+#    'description': """Quais Empresas Não Ativas na RFB mas Ativas na SEFAZ?""",
+#    'query': """
+#PREFIX sefazma: <http://www.sefaz.ma.gov.br/ontology/>
+#PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+#PREFIX owl: <http://www.w3.org/2002/07/owl#>
+#PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
+#PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#
+#SELECT ?estabelecimento_rfb ?nome_fantasia_rfb ?tipo_situacao_rfb ?tipo_situacao_cadastro WHERE {
+#    
+#  ?estabelecimento_rfb a sefazma:Estabelecimento_QC1_RFB;
+#              sefazma:nome_fantasia_rfb ?nome_fantasia_rfb;
+#     sefazma:tem_situacao_cadastral ?situacao_cadastral_rfb.
+#
+#  ?situacao_cadastral_rfb 
+#        sefazma:tipo_situacao ?tipo_situacao_rfb.
+#    
+#  ?estabelecimento_cadastro a sefazma:Estabelecimento_QC1_Cadastro;
+#      sefazma:nome_fantasia_cadastro ?nome_fantasia_cadastro;
+#      sefazma:tem_situacao_cadastral_cadastro ?situacao_cadastral_cadastro.
+#    
+#  ?estabelecimento_rfb owl:sameAs ?estabelecimento_cadastro.
+#    
+#  ?situacao_cadastral_cadastro 
+#      sefazma:tipo_situacao ?tipo_situacao_cadastro.
+# 
+#}
+#LIMIT 100""",
+#    'uri_var':"estabelecimento_rfb",
+#    'construct_query': """
+#    PREFIX sefazma: <http://www.sefaz.ma.gov.br/ontology/>
+#PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+#PREFIX owl: <http://www.w3.org/2002/07/owl#>
+#PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
+#PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#
+#CONSTRUCT{
+#    ?estabelecimento_rfb sefazma:tem_situacao_cadastral ?situacao_cadastral_rfb.
+#    ?situacao_cadastral_rfb sefazma:tipo_situacao ?tipo_situacao_rfb.
+#    ?estabelecimento_cadastro sefazma:tem_situacao_cadastral ?situacao_cadastral_cadastro.
+#    ?situacao_cadastral_cadastro sefazma:tipo_situacao ?tipo_situacao_cadastro.
+#    ?estabelecimento_rfb owl:sameAs  ?estabelecimento_cadastro.
+#    ?estabelecimento_cadastro owl:sameAs ?estabelecimento_rfb.
+#    
+#}WHERE {
+#  
+#  ?estabelecimento_rfb a sefazma:Estabelecimento_RFB;
+#     sefazma:tem_situacao_cadastral ?situacao_cadastral_rfb.
+#  
+#  ?situacao_cadastral_rfb sefazma:tipo_situacao ?tipo_situacao_rfb.
+#  
+#  ?estabelecimento_cadastro a sefazma:Estabelecimento_Cadastro;
+#        sefazma:tem_situacao_cadastral ?situacao_cadastral_cadastro.
+#    
+#  ?situacao_cadastral_cadastro sefazma:tipo_situacao ?tipo_situacao_cadastro.
+#    
+#  ?estabelecimento_rfb owl:sameAs  ?estabelecimento_cadastro.
+# 
+#    FILTER(?estabelecimento_rfb = <$URI>)
+#}
+#    """
+#}#
 
 
 q2 = {
@@ -718,4 +718,4 @@ CONSTRUCT{
     """
 }
 
-saved_queries = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15]
+saved_queries = [q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15]
