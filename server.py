b03fc33ea3cba0f8dd3807d556b95e3b6d33673c
“CaioViktor” <caioviktor@hotmail.com>
2022-11-23 12:31:33 -0300

    deploy

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import urllib.parse
 
 # ENDPOINT_ONTOLOGY = "http://localhost:7200/repositories/YOUR"#Endpoint for ontology
 # ENDPOINT_RESOURCES = "http://localhost:7200/repositories/YOUR"#Endpoint for resources
+# ENDPOINT_HISTORY = "http://localhost:7200/repositories/YOUR"#Endpoint for history
 # GRAPHDB_BROWSER = "http://localhost:7200/graphs-visualizations"#URL for browser graph-visualization
 # GRAPHDB_BROWSER_CONFIG = '' #set '' if uses default graph-visualization, '&config=ID' for custom graph-visualization config
 
@@ -16,6 +17,7 @@ ENDPOINT_ONTOLOGY = "http://10.33.96.18:7200/repositories/ONTOLOGIA_DOMINIO"
 ENDPOINT_RESOURCES = "http://10.33.96.18:7200/repositories/GRAFO_SEFAZMA_PRODUCAO"
 GRAPHDB_BROWSER = "http://10.33.96.18:7200/graphs-visualizations"
 GRAPHDB_BROWSER_CONFIG = "63b76b9865064cd8a9775e1e2f46ff4d"
+ENDPOINT_HISTORY = "http://10.33.96.18:7200/repositories/GRAFO_SEFAZMA_PRODUCAO"
 USE_N_ARY_RELATIONS = False
 
 # ENDPOINT_ONTOLOGY = "http://localhost:7200/repositories/ONTOLOGIA_DOMINIO"
